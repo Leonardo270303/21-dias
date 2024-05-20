398bec4e04633ce1e3fdbc407402a29f5ce2ac00
Leonardo Lobas <leonardolobas2703@gmail.com>
2024-05-20 19:59:25 -0300

    For Loop Exercicio 1

---
+++ Dia 6/script.js
@@ -0,0 +1,13 @@
+let numero = Number(prompt('Insira seu numero!'));
+for (let contador = 0; contador <= numero; contador++) {
+    console.log(contador);
+}
+
+
+for(let algoritimo = 0; algoritimo <= 10; algoritimo++) {
+    console.log(algoritimo * 5)
+}
+
+for(let algoritimo = 10; algoritimo >= 0; algoritimo--) {
+    console.log(algoritimo * 5)
+}
