22dc9669775d2b4d8400693e48392623f5e7a497
Leonardo Lobas <leonardolobas2703@gmail.com>
2024-05-20 20:26:09 -0300

    For loop exercicio 2

---
+++ Dia 6(2)/script.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt('Insira um número'))
+
+for(let contador = 0; contador <= 10; contador++) {
+    console.log(numero * contador)
+}
+for(let contador = 0; contador <= 10; contador++) {
+    console.log((numero + 1) * contador)
+}
+for(let contador = 0; contador <= 10; contador++) {
+    console.log((numero + 2)* contador)
+}
