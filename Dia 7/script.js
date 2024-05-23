e18c1fa691e20deed266ff12af14fdef096c55c0
LeonardoLobas <leonardolobas2703@gmail.com>
2024-05-23 17:23:15 -0300

    While and DoWhile.

---
+++ Dia 7/script.js
@@ -0,0 +1,11 @@
+let mediageral = 0
+let qtdHomens = 0
+let qtdMulheresAcimade7 = 0
+let maiorNotaHomens = 0
+let contador = 1
+
+
+while(contador <=10) {
+    nota = parseInt(prompt('Digite a nota do ' + contador + 'º aluno'))
+    sexo = prompt('Digite o sexo do aluno (m/f)')
+}
